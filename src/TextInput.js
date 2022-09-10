a5d4bfcaab9813ba0b4ad7d8128bc25484194e14
Facundo Ayosa <ayosafacundo@gmail.com>
2022-09-10 19:07:31 -0300

    markdown broken, need hotfix.

--- src/TextInput.js
+++ src/TextInput.js
@@ -1,33 +1,44 @@
 import { useState } from "react";
-
+import { marked } from 'marked';
+import * as DOMPurify from 'dompurify';
 function TextInput() {
   
   const [markdown, setMarkdown] = useState('');
-  var MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt({
-      html: true,
-      breaks: true
-    });
-  var preview = md.render(markdown);
+  
+  
+  // Copied from the CodePen
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+    pedantic: false,
+    gfm: true,
+    breaks: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    xhtml: false
+  });
+  // Source: https://codepen.io/freeCodeCamp/pen/GrZVVO?editors=0110
+
   const handleChange = e => {
     setMarkdown(e.target.value);
-    document.getElementById('previewer').innerHTML = preview;
+    document.getElementById('markdown').innerHTML = marked.parse(DOMPurify.sanitize(markdown));
   }
   //Markdown needs double space recognition so it gets replaced with <br/>
 
-  
+
   return (
-      <>
-        <section id="text-input">
-          <div id="header"> {/* make it pretty! */}
-            
-          </div>
-          <input type="text" id="rawMarkdown" name="rawMarkdown" onChange={handleChange} value={markdown}></input>
-        </section>
-        {/* make it pretty! */}
-        <section id="previewer">
-        </section>
-      </>
+    <>
+      <h1>Markdown Previewer</h1>
+      <section>
+
+        <p>Input Markdown</p>
+        <textarea id="editor" name="editor" onChange={handleChange} value={markdown}></textarea>
+
+        <p>Markdown Preview</p>
+      </section>
+      <div id="markdown"/>
+    </>
     );
 }
 export default TextInput;
